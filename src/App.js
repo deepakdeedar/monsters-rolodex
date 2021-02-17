65fc41969eff79b5e791b0da3c6b9f516f831072
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-02-17 22:36:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
 
     const { monsters, searchField } = this.state;
@@ -27,9 +31,10 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder= 'Search monster'
-          handleChange={e => this.setState({searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
